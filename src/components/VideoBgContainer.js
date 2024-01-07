58c5637ee7d8e97f15223fd926bcbac323a40cf7
Preeti Raj <satendra11kum@gmail.com>
2024-01-07 23:45:28 +0530

    add custom hook for fetch trailer video

--- src/components/VideoBgContainer.js
+++ src/components/VideoBgContainer.js
@@ -1,25 +1,11 @@
-import React, { useEffect } from "react";
-import { API_OPTION } from "../utils/constant";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../redux/moviesSlice";
+
+import {useSelector } from "react-redux";
+import useTrailerVideo from "../hooks/useTrailerVideo";
 
 const VideoBgContainer = ({ movieId}) => {
-  const dispatch = useDispatch();
   const TrailerId = useSelector(store => store.movies?.trailerVideo)
-  const getMovieVideo = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      API_OPTION
-    );
-    const json = await data.json();
-    console.log(json);
-    const filterData = json.results.filter((item) => item.type === "Trailer");
-    console.log("filterDAta", filterData);
-    dispatch(addTrailerVideo(filterData[0]));
-  };
-  useEffect(() => {
-    getMovieVideo();
-  }, []);
+  useTrailerVideo(movieId)
+  
   return (
     <div>
       <iframe
---
+++ src/hooks/useTrailerVideo.js
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../redux/moviesSlice";
+import { API_OPTION } from "../utils/constant";
+import { useEffect } from "react";
+
+const useTrailerVideo = (movieId) => {
+    const dispatch = useDispatch();
+    const getMovieVideo = async () => {
+        const data = await fetch(
+          `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+          API_OPTION
+        );
+        const json = await data.json();
+        console.log(json);
+        const filterData = json.results.filter((item) => item.type === "Trailer");
+        console.log("filterDAta", filterData);
+        dispatch(addTrailerVideo(filterData[0]));
+      };
+      useEffect(() => {
+        getMovieVideo();
+      }, []);
+}
+export default useTrailerVideo;
